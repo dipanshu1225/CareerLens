0f05301d6e141f8a2748418e42823f982c87b46c
Dipanshu Bhardwaj <dipanshubhardwaj1225@gmail.com>
2026-08-24 03:24:24 +0530

    Fix experience detection import

--- pages/resume_analyzer.py
+++ pages/resume_analyzer.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from pypdf import PdfReader
-from utils.parser import extract_text_from_pdf, detect_skills, detect_experience 
+from utils.parser import extract_text_from_pdf, detect_skills
 import re
 
 #for style css
